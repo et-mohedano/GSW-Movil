c5b75c6def0a1e585a2f30c55b524892c1571610
DylanDMMH <dylanmichel08011@gmail.com>
2021-12-04 08:25:43 -0600

    Logo y nombre
    Se agrego el nombre de la aplicación y se puso su logo como icono de la aplicación.

--- app/src/main/java/com/example/appmicalculadora/gswAdmProductos.java
+++ app/src/main/java/com/example/appmicalculadora/gswAdmProductos.java
@@ -3,12 +3,20 @@ package com.example.appmicalculadora;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class gswAdmProductos extends AppCompatActivity {
+
+    String resultadoId = "";
+    Button btnInsertarProducto, btnActualizarProducto, btnEliminarProducto;
+    EditText edtProducto, edt, edtDescripcion, edtPrecio, edtCantidad, edtActivo, edtFoto, edtStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gsw_adm_productos);
     }
+
+
 }
